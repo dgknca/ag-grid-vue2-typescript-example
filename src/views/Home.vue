84cb3817621f72f26c95032cf46353c5752da06f
dgknca <dgknca@gmail.com>
2020-09-08 00:50:19 +0300

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <ag-grid-vue
+      style="width: 500px; height: 500px;"
+      class="ag-theme-alpine"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+    ></ag-grid-vue>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue";
+
+@Component({
+  components: {
+    AgGridVue
+  }
+})
+export default class Home extends Vue {
+  columnDefs: any = null;
+  rowData: any = null;
+
+  beforeMount() {
+    this.columnDefs = [
+      { headerName: "Make", field: "make" },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
+    ];
+
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+  }
+}
+</script>
